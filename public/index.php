71986f8694f90a0880ed3b262cb16af2a179b179
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-18 09:50:05 -0300

    Primeiro commit. ConfiguraÃção de conexão com o banco de dados.

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once dirname(__FILE__, 2) . '/src/config/Database.php';
+//require_once __DIR__ . '/../src/config/Database.php';
+
+Database::getConnection();
---
+++ src/config/Database.php
@@ -0,0 +1,39 @@
+<?php
+
+class Database
+{
+  public static function getConnection()
+  {
+
+    $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+    $env = parse_ini_file($envPath);
+
+    $conn = new mysqli(
+      $env['host'],
+      $env['username'],
+      $env['password'],
+      $env['database']
+    );
+
+    if ($conn->connect_error) {
+      die("Erro " . $conn->connect_error);
+    }
+
+    return $conn;
+    /*
+    try {
+
+      $conn = new \PDO(
+        "mysql:host=" . $env['host'] .";dbname=" . $env['database'] .";charset=utf8",
+        $env['username'],
+        $env['password']
+      );
+
+      return $conn;
+
+    }catch (\PDOException $e) {
+
+    }
+    */
+  }
+}
