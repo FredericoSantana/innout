477f195625c69cfe0add47e57a335e996ca00d59
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-18 14:25:46 -0300

    Página de Login

--- public/index.php
+++ public/index.php
@@ -2,14 +2,5 @@
 
 //require_once dirname(__FILE__, 2) . '/src/config/Database.php';
 require_once dirname(__FILE__, 2) . '/src/config/config.php';
-require_once dirname(__FILE__, 2) . '/src/models/User.php';
+require_once dirname(__FILE__, 2) . '/src/views/login.php';
 
-$user = new User(['name' => 'Iara', 'email' => 'iara@gmail.com']);
-
-$q = User::getResultSetFromSelect(['id' => 1], 'name, email');
-
-var_dump(User::get(['name' => 'Chaves'], 'id, name, email'));
-
-foreach (User::get([], 'name') as $user) {
-  echo $user->name . '<br>';
-}
