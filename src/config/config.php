6564f1a5dcb8f1cac45e20264b9f5134c96d710e
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-18 15:22:45 -0300

    Login final

--- src/config/config.php
+++ src/config/config.php
@@ -5,8 +5,10 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 // Pastas do projeto
 define("MODEL_PATH", realpath(dirname(__FILE__) . '/../models'));
 define("VIEW_PATH", realpath(dirname(__FILE__) . '/../views'));
+define("CONTROLLER_PATH", realpath(dirname(__FILE__) . '/../controllers'));
 
 //Arquivo do banco de dados
 require_once realpath(dirname(__FILE__)) . '/Database.php';
+require_once realpath(dirname(__FILE__)) . '/loader.php';
 
 require_once (realpath(MODEL_PATH . '/Model.php'));
