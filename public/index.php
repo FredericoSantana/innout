c4c0041fef3998d31275b792d680dd57b844255c
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-18 14:51:09 -0300

    Implementando o Login

--- public/index.php
+++ public/index.php
@@ -2,5 +2,18 @@
 
 //require_once dirname(__FILE__, 2) . '/src/config/Database.php';
 require_once dirname(__FILE__, 2) . '/src/config/config.php';
-require_once dirname(__FILE__, 2) . '/src/views/login.php';
+//require_once VIEW_PATH . '/login.php';
 
+require_once MODEL_PATH . '/Login.php';
+
+$login = new Login([
+  'email' => 'quico@cod3r.com.br',
+  'password' => 'a'
+]);
+
+try {
+  $login->checkLogin();
+  echo 'Logado com sucesso';
+}catch (Exception $e){
+  echo 'Problema no login';
+}
--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,9 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 // Pastas do projeto
 define("MODEL_PATH", realpath(dirname(__FILE__) . '/../models'));
+define("VIEW_PATH", realpath(dirname(__FILE__) . '/../views'));
 
 //Arquivo do banco de dados
 require_once realpath(dirname(__FILE__)) . '/Database.php';
+
+require_once (realpath(MODEL_PATH . '/Model.php'));
